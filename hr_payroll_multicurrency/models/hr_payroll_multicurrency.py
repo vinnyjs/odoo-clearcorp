b4b18a6110bac135e3c038e4fb0d9fc6cc69530d
vinitoreto@gmail.com <vinitoreto@gmail.com>
2017-07-28 19:52:22 -0600

    Se migra módulo a la V 10.0

---
+++ hr_payroll_multicurrency/models/hr_payroll_multicurrency.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2016 ClearCorp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openerp import models, fields, api
+
+
+class HRContract(models.Model):
+    _inherit = 'hr.contract'
+
+    currency_id = fields.Many2one('res.currency', string='Currency')
+
+
+class HRPayslip(models.Model):
+    _inherit = 'hr.payslip'
+
+    currency_id = fields.Many2one('res.currency', compute='_compute_currency',
+                                  string='Currency')
+
+    @api.depends('contract_id')
+    @api.one
+    def _compute_currency(self):
+        self.currency_id = self.contract_id.currency_id
